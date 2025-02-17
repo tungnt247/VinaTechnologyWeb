ac2b0773443d9540d8ecab848e429706dc26493f
DevMinhThu <devminhthu2023@gmail.com>
2025-02-18 00:18:41 +0700

    feat/add-i18n

--- resources/js/load-footer.js
+++ resources/js/load-footer.js
@@ -3,6 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("footer").innerHTML = data;
+      updateFooterContent();
     })
     .catch((error) => console.error("Error loading footer:", error));
 });
+
+function updateFooterContent() {
+  const footerElements = document.querySelectorAll("#footer [data-i18n]");
+  footerElements.forEach((el) => {
+    const key = el.getAttribute("data-i18n");
+    el.innerHTML = i18next.t(key);
+  });
+}
--- resources/js/load-header-other.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("/resources/components/header-other.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("header-other").innerHTML = data;
-    })
-    .catch((error) => console.error("Error loading header:", error));
-});
